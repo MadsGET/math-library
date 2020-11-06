d18e655709b13be7dd2c25a22dcb074e8072a0f6
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-11-06 14:16:59 +0100

    Update math.js

--- math.js
+++ math.js
@@ -1,5 +1,14 @@
 const mathf =
 {
+	time:
+	{
+		// Formats an integer to time format.
+		formatNumber(int)
+		{
+			return (int < 10) ? '0' + int : String(int);
+		}
+	},
+
 	// Checks if a number is within a certain range. 
 	withinRange(value, min, max)
 	{
