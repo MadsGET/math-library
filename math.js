cfcfbe3faf7623ea4432422c9ee376484dbcebf3
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-11-07 12:28:29 +0100

    Update math.js

--- math.js
+++ math.js
@@ -2,11 +2,20 @@ const mathf =
 {
 	time:
 	{
+		// Returns minutes as hour and minutes.
+		convertMinutes(value)
+		{
+			let converted = value / 60;
+			let hours = Math.floor(converted);
+			let minutes = Math.round((converted - hours) * 60);
+			return { hours: hours, minutes: minutes };
+		},
+
 		// Formats an integer to time format.
 		formatNumber(int)
 		{
 			return (int < 10) ? '0' + int : String(int);
-		}
+		},
 	},
 
 	// Checks if a number is within a certain range. 
