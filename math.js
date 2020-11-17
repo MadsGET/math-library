87f129eeca12dd8212a847fe66e979efe008e286
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-11-17 12:20:17 +0100

    Clamp function

--- math.js
+++ math.js
@@ -1,26 +1,24 @@
 const mathf =
 {
-	time:
-	{
-		// Returns minutes as hour and minutes.
-		convertMinutes(value)
-		{
-			let converted = value / 60;
-			let hours = Math.floor(converted);
-			let minutes = Math.round((converted - hours) * 60);
-			return { hours: hours, minutes: minutes };
-		},
-
-		// Formats an integer to time format.
-		formatNumber(int)
-		{
-			return (int < 10) ? '0' + int : String(int);
-		},
+	// Checks if a number is within a certain range. 
+	withinRange(value, min, max) {
+		return (value >= min && value <= max);
 	},
 
-	// Checks if a number is within a certain range. 
-	withinRange(value, min, max)
+	// Clamps a value between a and b.
+	clamp(value, min, max)
 	{
-		return (value >= min && value <= max);
-	}
+		if (value < min)
+		{
+			return min;
+		}
+		else if (value > max)
+		{
+			return max;
+		}
+		else
+		{
+			return value;
+		}
+	},
 }
